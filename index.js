ae6121eadb975248d7b33cba50a15a42116de276
IAmNini <fortuny.carrega@gmail.com>
2023-02-22 16:56:54 +0000

    Authenticate successfully

--- index.js
+++ index.js
@@ -5,54 +5,53 @@ const app = express();
 app.use(express.static("public"));
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-const credentials = {username: 'admin', password: 'admin'};
-let code;
+const credentials = { username: "admin", password: "admin" };
+let storedCode = {};
+let session = {};
 
-app.get('/oauth/authorize', (req, res) => {
-  // check that credentials are correct
-  // store code in code variable (with username)
-  // redirect to the client app with query params
-})
+app.get("/oauth/authorize", (req, res) => {
+  const { code, redirect_uri, username, state } = req.query;
+  storedCode[username] = code;
+  res.redirect(301, `${redirect_uri}?code=${code}&state=${state}`);
+});
 
-app.get('/oauth/token', (req, res) => {
-  // if the cookie exists and is authenticated, send back access token
-  // else, check that code from query params matches previously stored code
-  // if yes, send back access token and set session cookie
-  // else, send back 401
-  // start coding the comments
+app.post("/oauth/token", (req, res) => {
+  const { username, code } = req.query;
 
-  const token = {access_token: "token", token_type: 'Bearer', expires_in: 86400}
+  const token = {
+    access_token: "token",
+    token_type: "Bearer",
+    expires_in: 86400,
+  };
 
-  if (req.cookies.session) {
-    // logic
+  if (req.cookies && session && session[username]) {
     res.json(token);
-    
   } else {
-    if (req.query.code === code) {
+    if (code === storedCode[username]) {
+      session[username] = true;
       // send back access token
-      res.cookie('session', 'authenticated', { httpOnly: true });
+      res.cookie("session", "authenticated", { httpOnly: true });
       res.json(token);
     } else {
       // send back 401
-      res.status(401).send('Unauthorized');
+      res.status(401).send("Unauthorized");
     }
   }
-})
+});
 
-app.get('/api/users', (req, res) => {
-  res.json({users: [{name: 'John'}, {name: 'Jane'}]});
-})
+app.get("/api/users", (req, res) => {
+  res.json({ users: [{ name: "John" }, { name: "Jane" }] });
+});
 
-app.get('/random', (req, res) => {
-  res.json([{name: 'Random1'}, {name: 'Random2'}]);
-})
-
+app.get("/random", (req, res) => {
+  res.json([{ name: "Random1" }, { name: "Random2" }]);
+});
 
 // listen to port 3000
 app.listen(3000, () => {
-  console.log('Server started on port 3000');
-})
+  console.log("Server started on port 3000");
+});
--- public/oauth2.js
+++ public/oauth2.js
@@ -2,7 +2,9 @@
 function generateRandomString() {
   const array = new Uint32Array(28);
   window.crypto.getRandomValues(array);
-  return Array.from(array, dec => ('0' + dec.toString(16)).substr(-2)).join('');
+  return Array.from(array, (dec) => ("0" + dec.toString(16)).substr(-2)).join(
+    ""
+  );
 }
 
 // Calculate the SHA256 hash of the input text.
@@ -10,19 +12,15 @@ function generateRandomString() {
 function sha256(plain) {
   const encoder = new TextEncoder();
   const data = encoder.encode(plain);
-  return window.crypto.subtle.digest('SHA-256', data);
+  return window.crypto.subtle.digest("SHA-256", data);
 }
 
 // Base64-urlencodes the input string
 function base64urlencode(str) {
-   // Encode the string as base64
-   const base64 = Buffer.from(str).toString('base64');
-
-   // Replace '+' and '/' with '-' and '_' respectively
-   const base64Url = base64.replace(/\+/g, '-').replace(/\//g, '_');
- 
-   // Remove any trailing '=' characters
-   return base64Url.replace(/=+$/, '');
+  return btoa(String.fromCharCode.apply(null, new Uint8Array(str)))
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=+$/, "");
 }
 
 // Return the base64-urlencoded sha256 hash for the PKCE
@@ -32,29 +30,40 @@ async function pkceChallengeFromVerifier(v) {
   return base64urlencode(hashed);
 }
 
-export async function authCodeRequest({client_id, redirect_uri, authorization_endpoint, requested_scopes}) {
-
+export async function authCodeRequest({
+  client_id,
+  redirect_uri,
+  authorization_endpoint,
+  requested_scopes,
+}) {
   // Create and store a random "state" value
-const initState = generateRandomString();
-localStorage.setItem("pkce_state", initState);
-console.log("initState:", initState)
+  const initState = generateRandomString();
+  localStorage.setItem("pkce_state", initState);
+  console.log("initState:", initState);
 
-// Create and store a new PKCE code_verifier (the plaintext random secret)
-const code_verifier = generateRandomString();
-localStorage.setItem("pkce_code_verifier", code_verifier);
+  // Create and store a new PKCE code_verifier (the plaintext random secret)
+  const code_verifier = generateRandomString();
+  localStorage.setItem("pkce_code_verifier", code_verifier);
 
-// Hash and base64-urlencode the secret to use as the challenge
-const code_challenge = await pkceChallengeFromVerifier(code_verifier);
+  // Hash and base64-urlencode the secret to use as the challenge
+  const code_challenge = await pkceChallengeFromVerifier(code_verifier);
 
-// Build the authorization URL
-const url = authorization_endpoint
-  + "?response_type=code"
-  + "&client_id="+encodeURIComponent(client_id)
-  + "&state="+encodeURIComponent(initState)
-  + "&scope="+encodeURIComponent(requested_scopes)
-  + "&redirect_uri="+encodeURIComponent(redirect_uri)
-  + "&code_challenge="+encodeURIComponent(code_challenge)
-+ "&code_challenge_method=S256";
+  // Build the authorization URL
+  const url =
+    authorization_endpoint +
+    "?response_type=code" +
+    "&client_id=" +
+    encodeURIComponent(client_id) +
+    "&state=" +
+    encodeURIComponent(initState) +
+    "&scope=" +
+    encodeURIComponent(requested_scopes) +
+    "&redirect_uri=" +
+    encodeURIComponent(redirect_uri) +
+    "&code_challenge=" +
+    encodeURIComponent(code_challenge) +
+    "&code_challenge_method=S256" +
+    "&username=admin";
 
-window.location = url;
+  window.location = url;
 }
